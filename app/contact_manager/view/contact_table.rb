7fd10739745ef43b256af9a1a21e739a26e38aaf
Andy Maleh <andy.am@gmail.com>
2022-05-29 14:14:22 -0400

    contact_table search functionality

--- app/contact_manager/view/contact_table.rb
+++ app/contact_manager/view/contact_table.rb
@@ -3,7 +3,7 @@ class ContactManager
     class ContactTable
       include Glimmer::UI::CustomWidget
       
-      attr_accessor :contacts
+      attr_accessor :contacts, :query
     
       before_body do
         self.contacts = Contact.all
@@ -18,31 +18,60 @@ class ContactManager
       end
             
       body {
-        table {
-          table_column {
-            text 'First Name'
-            width 120
-          }
-          table_column {
-            text 'Last Name'
-            width 120
-          }
-          table_column {
-            text 'Email'
-            width 150
-          }
-          table_column {
-            text 'Phone'
-            width 120
-          }
-          table_column {
-            text 'Address'
-            width 320
+        composite {
+          grid_layout
+          
+          text(:search) {
+            layout_data {
+              horizontal_alignment :fill
+              grab_excess_horizontal_space true
+            }
+            
+            text <=> [self, :query,
+              after_write: ->(query_value) {
+                if query_value.present?
+                  attribute_names = Contact.new.attributes.keys
+                  conditions = attribute_names.reduce('') do |conditions, attribute|
+                    if conditions.blank?
+                      conditions += "lower(#{attribute}) like ?"
+                    else
+                      conditions += " OR lower(#{attribute}) like ?"
+                    end
+                  end
+                  self.contacts = Contact.where(conditions, *(["%#{query_value.downcase}%"]*attribute_names.count))
+                else
+                  self.contacts = Contact.all
+                end
+              }
+            ]
           }
           
-          # Ensure converting to Array on read because contacts is an ActiveRecord collection,
-          # but an Array object is required by Glimmer DSL for SWT table data-binding logic
-          items <= [self, :contacts, on_read: :to_a, column_properties: [:first_name, :last_name, :email, :phone, :address]]
+          table {
+            table_column {
+              text 'First Name'
+              width 120
+            }
+            table_column {
+              text 'Last Name'
+              width 120
+            }
+            table_column {
+              text 'Email'
+              width 150
+            }
+            table_column {
+              text 'Phone'
+              width 120
+            }
+            table_column {
+              text 'Address'
+              width 320
+            }
+            
+            # Ensure converting to Array on read because contacts is an ActiveRecord collection,
+            # but an Array object is required by Glimmer DSL for SWT table data-binding logic
+            items <= [self, :contacts, on_read: :to_a, column_properties: [:first_name, :last_name, :email, :phone, :address]]
+          }
         }
       }
     end
