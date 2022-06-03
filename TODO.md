067091f41bd326485676c9dd4a57b1adca2bed21
Andy Maleh <andy.am@gmail.com>
2022-06-03 11:26:04 -0400

    Improve phone validation

--- TODO.md
+++ TODO.md
@@ -1,6 +1,5 @@
 # TODO
 
-- Better Phone validation
 - Upgrade to JRuby 9.3.4.0
 - Upgrade to latest glimmer-dsl-swt (after making a release with fixes)
 - Animated gif screenshot
--- app/contact_manager/model/contact.rb
+++ app/contact_manager/model/contact.rb
@@ -11,7 +11,7 @@ class Contact < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, format: {with: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}\b/i, message: 'must be a valid email address'}, allow_blank: true
-  validates :phone, format: {with: /\A[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.\/0-9]*\z/, message: 'must be a valid phone number'}, allow_blank: true
+  validates :phone, format: {with: /\A[(]?[0-9]{3}[)]?[ -\.\/0-9]{7,9}\z/, message: 'must be a valid phone number'}, allow_blank: true
   validates :zip_or_postal_code, format: {with: /\A[a-z0-9][0-9][a-z0-9][ 0-9][a-z0-9][a-z0-9]?[0-9]?\z/i, message: 'must be a valid phone number'}, allow_blank: true
   validate :email_or_phone_is_present
   
