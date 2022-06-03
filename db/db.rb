16355cd8f1611a8415ebe71e92513b4a3de97cee
Andy Maleh <andy.am@gmail.com>
2022-06-03 12:25:52 -0400

    Rename database file

--- db/db.rb
+++ db/db.rb
@@ -2,5 +2,8 @@ require 'active_record'
 require 'jdbc/sqlite3'
 Jdbc::SQLite3.load_driver
 require 'activerecord-jdbcsqlite3-adapter' if defined? JRUBY_VERSION
-@connection = ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: File.join(Dir.home, 'db/database.sqlite3'))
+@connection = ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: File.join(Dir.home, 'db/contact_manager.sqlite3')
+)
 require 'db/migrate'
